f1c3ddde60d3ef398b4fed3fdf533c90e135c69e
Seraphic <seraphic@192.168.1.14>
2025-09-18 15:39:59 +0530

    home changed

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -5,15 +5,14 @@ use Psr\Log\LoggerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-use App\Models\Users;
-
 class HomeController
 {
     public function index(Request $request, Response $response): Response
     {
-        $users = Users::all();
 
-        $payload = json_encode($users, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        $payload = json_encode([
+            'AppName' => 'Slim App'
+        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
         $response->getBody()->write($payload);
 
 
